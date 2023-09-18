ef24b7270ea69e8bf7b51efd8607d7a67257fc71
v2ray <2282688304@qq.com>
2023-09-18 03:22:19 +0000

    Added SchizoGPT training script.

---
+++ scripts/finetune_schizogpt_13b.sh
@@ -0,0 +1,46 @@
+NCCL_P2P_DISABLE=1 accelerate launch --num_processes 8 --num_cpu_threads_per_process 30 flash_qlora.py \
+    --ddp_find_unused_parameters False \
+    --model_name_or_path TheBloke/Llama-2-13B-fp16 \
+    --output_dir ./output/SchizoGPT-13B-QLoRA \
+    --logging_steps 10 \
+    --save_strategy steps \
+    --data_seed 42 \
+    --save_steps 69420 \
+    --save_total_limit 10 \
+    --evaluation_strategy steps \
+    --eval_dataset_size 32 \
+    --max_eval_samples 1000 \
+    --per_device_eval_batch_size 1 \
+    --max_new_tokens 32 \
+    --dataloader_num_workers 30 \
+    --group_by_length \
+    --logging_strategy steps \
+    --remove_unused_columns False \
+    --do_train \
+    --do_eval \
+    --do_mmlu_eval False \
+    --lora_r 64 \
+    --lora_alpha 16 \
+    --lora_modules all \
+    --double_quant \
+    --quant_type nf4 \
+    --bf16 \
+    --bits 4 \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type cosine \
+    --adam8bit \
+    --gradient_checkpointing \
+    --dataset /home/ubuntu/r-chatgpt-general-dump/merged_strings.jsonl \
+    --model_max_len 4096 \
+    --source_max_len 1 \
+    --target_max_len 4095 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --max_steps 500 \
+    --eval_steps 50 \
+    --learning_rate 0.0002 \
+    --adam_beta2 0.999 \
+    --max_grad_norm 0.3 \
+    --lora_dropout 0.1 \
+    --weight_decay 0.0 \
+    --seed 0
